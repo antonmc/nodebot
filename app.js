0743a638924aada28484c96f0ad5df21557f529f
antonm <antonm@ca.ibm.com>
2017-08-28 11:18:04 -0300

    updates

--- app.js
+++ app.js
@@ -34,193 +34,179 @@ var chatbot = require('./bot.js');
 //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
 
 function calcCrow(lat1, lon1, lat2, lon2) {
-  let R = 6371; // km
-  let dLat = toRad(lat2 - lat1);
-  let dLon = toRad(lon2 - lon1);
-  lat1 = toRad(lat1);
-  lat2 = toRad(lat2);
+    let R = 6371; // km
+    let dLat = toRad(lat2 - lat1);
+    let dLon = toRad(lon2 - lon1);
+    lat1 = toRad(lat1);
+    lat2 = toRad(lat2);
 
-  let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  let d = R * c;
-  return d;
+    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = R * c;
+    return d;
 }
 
 // Converts numeric degrees to radians
 function toRad(Value) {
-  return Value * Math.PI / 180;
+    return Value * Math.PI / 180;
 }
 
 // Set geographic location
 
-app.post('/location', function(req, res) {
+app.post('/location', function (req, res) {
 
-  console.log('setting geographic location');
+    console.log('setting geographic location');
 
-  res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json');
 
-  latitude = parseFloat(req.body.latitude);
-  longitude = parseFloat(req.body.longitude);
+    latitude = parseFloat(req.body.latitude);
+    longitude = parseFloat(req.body.longitude);
 
-  res.send(JSON.stringify({
-    outcome: "success"
-  }, null, 3));
+    res.send(JSON.stringify({
+        outcome: "success"
+    }, null, 3));
 
 });
 
-app.post('/outpost', function(req, res) {
+app.post('/outpost', function (req, res) {
 
-  res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json');
 
-  console.log('dorinha heard a question ... ');
+    console.log('dorinha heard a question ... ');
 
-  if (req.body.drug) {
+    if (req.body.drug) {
 
-    console.log('this is where we look for a clinic');
+        console.log('this is where we look for a clinic');
 
-    var clinics = 'https://gateway-cidadao.campinas.sp.gov.br/api/v1/remedios/centros/saude';
-    clinics = clinics + '?id_remedio=' + req.body.drug.id;
+        var clinics = 'https://gateway-cidadao.campinas.sp.gov.br/api/v1/remedios/centros/saude';
+        clinics = clinics + '?id_remedio=' + req.body.drug.id;
 
-    var options = {
-      url: clinics,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla'
-      },
-      json: null
-    };
-
-    request(options, function(err, newresponse, clinics) {
-
-      console.log(newresponse.body);
-
-      if (newresponse && (newresponse.statusCode === 200 || newresponse.statusCode === 201)) {
-
-        var closestIndex = 0;
-
-        var postoDeSaudes = JSON.parse(newresponse.body);
-
-        console.log('testing data structure')
-
-        console.log(postoDeSaudes[0]);
-
-        for (var count = 0; count < postoDeSaudes.length; count++) {
-
-          var clinicLat = postoDeSaudes[count].lat;
-          var clinicLng = postoDeSaudes[count].lng;
-
-          // console.log(postoDeSaudes[count]);
-          //
-          // console.log(clinicLat);
-          // console.log(clinicLng);
-          //
-          // console.log(latitude);
-          // console.log(longitude);
-
-          postoDeSaudes[count].distance = calcCrow(latitude, longitude, clinicLat, clinicLng );
-
-          if(postoDeSaudes[count].distance < postoDeSaudes[closestIndex].distance ){
-              closestIndex = count;
-          }
-
-          // console.log(distance);
-        }
-
-        console.log('closest clinic: ' + postoDeSaudes[closestIndex].nome )
-
-        // console.log(clinics);
-      }
-    })
-  } else {
-
-    chatbot.sendMessage(req.body.text, req.body.context, function(response) {
-
-      // console.log(response);
-
-      if (response.entities.length > 0) {
-
-        if (response.entities[0].entity === 'druglist') {
-          console.log('here is where to search for the drugs');
-
-          var remedies = 'https://gateway-cidadao.campinas.sp.gov.br/api/v1/remedios';
-
-          var input = response.input.text.toUpperCase();
-
-          input = input.replace(/[|&;$%@"<>()+,]/g, "");
-
-          console.log(input);
-
-          var json = {
-            "name": "Test",
-            "email": "test@xxxx.in",
-            "phone": "989898xxxx"
-          };
-
-          var options = {
-            url: remedies,
+        var options = {
+            url: clinics,
             method: 'GET',
             headers: {
-              'Content-Type': 'application/json',
-              'User-Agent': 'Mozilla'
+                'Content-Type': 'application/json',
+                'User-Agent': 'Mozilla'
             },
-            json: json
-          };
+            json: null
+        };
 
-          request(options, function(err, newresponse, medicines) {
+        request(options, function (err, newresponse, clinics) {
+
+            console.log(newresponse.body);
+
+            var clinicData;
+
             if (newresponse && (newresponse.statusCode === 200 || newresponse.statusCode === 201)) {
-              // console.log(response);
 
-              var count = 1;
+                var closestIndex = 0;
 
-              var findings = 'Which medicine are you looking for:\n';
+                var postoDeSaudes = JSON.parse(newresponse.body);
 
-              var drugs = [];
+                console.log('testing data structure')
 
-              medicines.forEach(function(medicine) {
+                console.log(postoDeSaudes[0]);
 
-                // console.log(medicine.label);
+                for (var count = 0; count < postoDeSaudes.length; count++) {
 
-                //  input = 'DIPIRONA';
+                    var clinicLat = postoDeSaudes[count].lat;
+                    var clinicLng = postoDeSaudes[count].lng;
 
-                if (medicine.label.indexOf(input) !== -1) {
+                    postoDeSaudes[count].distance = calcCrow(latitude, longitude, clinicLat, clinicLng);
 
-                  drugs.push(medicine);
-                  // findings = findings + count + ': ' + medicine.label + '\n';
-                  count++;
+                    if (postoDeSaudes[count].distance < postoDeSaudes[closestIndex].distance) {
+                        closestIndex = count;
+                    }
+
+                    clinicData = postoDeSaudes[closestIndex];
                 }
-              })
 
-              console.log(findings);
+                var response = {};
 
-              response.output.text[0] = findings;
+                response.output = {};
 
-              response.drugs = drugs;
+                response.output.text = 'The closest clinic is: ';
+                response.context = {};
 
-              res.send(JSON.stringify(response, null, 3));
+                response.map = clinicData;
+
+                console.log('closest clinic: ' + postoDeSaudes[closestIndex].nome);
+
+                console.log(response);
+
+                res.send(JSON.stringify(response, null, 3));
             }
-          });
-        }
-      } else {
-        res.send(JSON.stringify(response, null, 3));
-      }
+        })
+    } else {
 
-      // switch (response.intents[0].intent) {
-      //     default: console.log('case statement - default - ' + response.intents[0].intent);
-      //     break;
-      // }
+        chatbot.sendMessage(req.body.text, req.body.context, function (response) {
 
-      // if( response.entities[0] !== null && response.entities[0].entity === 'druglist'){
-      //   console.log('here is where to search for the drugs');
-      // }
+            // console.log(response);
 
-      // res.send(JSON.stringify(response, null, 3));
-    });
-  }
+            if (response.entities.length > 0) {
+
+                if (response.entities[0].entity === 'druglist') {
+                    console.log('here is where to search for the drugs');
+
+                    var remedies = 'https://gateway-cidadao.campinas.sp.gov.br/api/v1/remedios';
+
+                    var input = response.input.text.toUpperCase();
+
+                    input = input.replace(/[|&;$%@"<>()+,]/g, "");
+
+                    console.log(input);
+
+                    var json = {};
+
+                    var options = {
+                        url: remedies,
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'User-Agent': 'Mozilla'
+                        },
+                        json: json
+                    };
+
+                    request(options, function (err, newresponse, medicines) {
+                        if (newresponse && (newresponse.statusCode === 200 || newresponse.statusCode === 201)) {
+                            // console.log(response);
+
+                            var count = 1;
+
+                            var findings = 'Which medicine are you looking for:\n';
+
+                            var drugs = [];
+
+                            medicines.forEach(function (medicine) {
+
+                                if (medicine.label.indexOf(input) !== -1) {
+
+                                    drugs.push(medicine);
+                                    // findings = findings + count + ': ' + medicine.label + '\n';
+                                    count++;
+                                }
+                            })
+
+                            console.log(findings);
+
+                            response.output.text[0] = findings;
+
+                            response.drugs = drugs;
+
+                            res.send(JSON.stringify(response, null, 3));
+                        }
+                    });
+                }
+            } else {
+                res.send(JSON.stringify(response, null, 3));
+            }
+        });
+    }
 });
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+app.listen(appEnv.port, '0.0.0.0', function () {
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
 });
--- public/src/nodebot.js
+++ public/src/nodebot.js
@@ -5,11 +5,11 @@ var drugs;
 
 function initialize() {
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(sendLocationToServer);
-  } else {
-    console.log("Geolocation is not supported by this browser");
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(sendLocationToServer);
+    } else {
+        console.log("Geolocation is not supported by this browser");
+    }
 }
 
 function sendLocationToServer(position) {
@@ -44,11 +44,10 @@ function sendLocationToServer(position) {
     xhr.send(JSON.stringify(location));
 }
 
-
 function resize(event) {}
 
 function handleFocus(event) {
-  console.log('focus');
+    console.log('focus');
 }
 
 /**
@@ -60,128 +59,171 @@ function handleFocus(event) {
  * @param {Object} e - Information about the keyboard event.
  */
 function handleInput(e) {
-  // Only check for a return/enter press - Event 13
-  if (e.which === 13 || e.keyCode === 13) {
+    // Only check for a return/enter press - Event 13
+    if (e.which === 13 || e.keyCode === 13) {
 
-    var userInput = document.getElementById('chatMessage');
-    text = userInput.value; // Using text as a recurring variable through functions
-    text = text.replace(/(\r\n|\n|\r)/gm, ""); // Remove erroneous characters
+        var userInput = document.getElementById('chatMessage');
+        text = userInput.value; // Using text as a recurring variable through functions
+        text = text.replace(/(\r\n|\n|\r)/gm, ""); // Remove erroneous characters
 
-    // If there is any input then check if this is a claim step
-    // Some claim steps are handled in newEvent and others are handled in userMessage
-    if (text) {
+        // If there is any input then check if this is a claim step
+        // Some claim steps are handled in newEvent and others are handled in userMessage
+        if (text) {
 
-      if (drugs !== undefined) {
-        var index = parseInt(text);
-        personBubble(text);
-        userInput.value = '';
-        sendMessageToWatson(drugs[index].label, drugs[index]);
-        drugs = null;
-      } else {
+            if (drugs !== undefined) {
+                var index = parseInt(text);
+                personBubble(text);
+                userInput.value = '';
+                sendMessageToWatson(drugs[index].label, drugs[index]);
+                drugs = null;
+            } else {
 
-        // Display the user's text in the chat box and null out input box
-        personBubble(text);
-        userInput.value = '';
+                // Display the user's text in the chat box and null out input box
+                personBubble(text);
+                userInput.value = '';
 
-        sendMessageToWatson(text);
-      }
+                sendMessageToWatson(text);
+            }
 
-    } else {
+        } else {
 
-      // Blank user message. Do nothing.
-      console.error("No message.");
-      userInput.value = '';
+            // Blank user message. Do nothing.
+            console.error("No message.");
+            userInput.value = '';
 
-      return false;
+            return false;
+        }
     }
-  }
 }
 
 function sendMessageToWatson(message, drug) {
 
-  // Set parameters for payload to Watson Conversation
+    // Set parameters for payload to Watson Conversation
 
-  params.text = message; // User defined text to be sent to service
-  if (drug) {
-    params.drug = drug;
-  }
-
-  if (context) {
-    params.context = context;
-  }
-
-  var xhr = new XMLHttpRequest();
-  var uri = '/outpost';
-
-  xhr.open('POST', uri, true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = function() {
-
-    // Verify if there is a success code response and some text was sent
-    if (xhr.status === 200 && xhr.responseText) {
-
-      var response = JSON.parse(xhr.responseText);
-      text = response.output.text; // Only display the first response
-      context = response.context; // Store the context for next round of questions
-
-      console.log('context: ' + JSON.stringify(response));
-
-      console.log("Got response from Watson: ", text[0]);
-
-      scoutBubble(text[0]);
-
-      drugs = response.drugs
-
-      var count = 1;
-
-      if (response.drugs) {
-
-        response.drugs.forEach(function(drug) {
-          scoutBubble(count + ': ' + drug.label);
-          count++;
-        })
-      }
-
-    } else {
-      console.error('Server error for Conversation. Return status of: ', xhr.statusText);
+    params.text = message; // User defined text to be sent to service
+    if (drug) {
+        params.drug = drug;
     }
-  };
 
-  xhr.onerror = function() {
-    console.error('Network error trying to send message!');
-  };
+    if (context) {
+        params.context = context;
+    }
 
-  console.log(JSON.stringify(params));
+    var xhr = new XMLHttpRequest();
+    var uri = '/outpost';
 
-  xhr.send(JSON.stringify(params));
+    xhr.open('POST', uri, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+
+        // Verify if there is a success code response and some text was sent
+        if (xhr.status === 200 && xhr.responseText) {
+
+            var response = JSON.parse(xhr.responseText);
+            text = response.output.text; // Only display the first response
+            context = response.context; // Store the context for next round of questions
+
+            console.log('context: ' + JSON.stringify(response));
+
+            console.log("Got response from Watson: ", text[0]);
+
+            scoutBubble(text[0]);
+
+            //            mapBubble(text[0]);
+
+            drugs = response.drugs
+
+            var count = 1;
+
+            if (response.drugs) {
+
+                response.drugs.forEach(function (drug) {
+                    scoutBubble(count + ': ' + drug.label);
+                    count++;
+                })
+            }
+
+            if (response.map) {
+                scoutBubble(response.output.text);
+                scoutBubble(response.map.nome);
+                mapBubble();
+            }
+
+        } else {
+            console.error('Server error for Conversation. Return status of: ', xhr.statusText);
+        }
+    };
+
+    xhr.onerror = function () {
+        console.error('Network error trying to send message!');
+    };
+
+    console.log(JSON.stringify(params));
+
+    xhr.send(JSON.stringify(params));
 }
 
 function personBubble(message) {
-  console.log(message);
-  var bubble = document.createElement('div');
-  bubble.className = 'person';
-  bubble.innerHTML = '<div class = "person-content">' + message + '</div>';
+    console.log(message);
+    var bubble = document.createElement('div');
+    bubble.className = 'person';
+    bubble.innerHTML = '<div class = "person-content">' + message + '</div>';
 
-  var conversation = document.getElementById('conversationflow');
-  conversation.appendChild(bubble);
-  conversation.scrollTop = conversation.scrollHeight;
+    var conversation = document.getElementById('conversationflow');
+    conversation.appendChild(bubble);
+    conversation.scrollTop = conversation.scrollHeight;
 }
 
 function scoutBubble(message) {
-  console.log(message);
-  var bubble = document.createElement('div');
-  bubble.className = 'scoutbot';
-  bubble.innerHTML = '<div class = "scoutbot-content">' + message + '</div>';
+    console.log(message);
+    var bubble = document.createElement('div');
+    bubble.className = 'scoutbot';
+    bubble.innerHTML = '<div class = "scoutbot-content">' + message + '</div>';
 
-  var conversation = document.getElementById('conversationflow');
-  conversation.appendChild(bubble);
-  conversation.scrollTop = conversation.scrollHeight;
+    var conversation = document.getElementById('conversationflow');
+    conversation.appendChild(bubble);
+    conversation.scrollTop = conversation.scrollHeight;
 }
 
-function mapBubble(element){
-  console.log('map bubble');
-  var bubble = document.createElement('div');
-  bubble.className = 'mapfragment';
-  bubble.innerHTML = '<div id="' + Math.random() + 'map"></div>'
+function initMap(id) {
+    var myLatLng = {
+        lat: -25.363,
+        lng: 131.044
+    };
 
+    var map = new google.maps.Map(document.getElementById(id), {
+        zoom: 4,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+
+    setTimeout(function () {
+        google.maps.event.trigger(map, 'resize')
+    }, 600);
+}
+
+
+function mapBubble(message) {
+    console.log('map bubble');
+
+    var bubble = document.createElement('div');
+
+    bubble.className = 'mapfragment';
+
+    var id = Math.random() + "map";
+
+    bubble.innerHTML = '<div height=200; width=200; id="' + id + '"></div>'
+
+    var conversation = document.getElementById('conversationflow');
+
+    conversation.appendChild(bubble);
+
+    initMap(id);
+
+    conversation.scrollTop = conversation.scrollHeight;
 }
